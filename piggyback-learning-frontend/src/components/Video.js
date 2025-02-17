3edad9108700d6731e5149dc785adc032bdece3b
Gabriel Lantigua <143743234+glantig1@users.noreply.github.com>
2025-02-17 04:38:08 +0000

    implemented proper pause/play button using refactored code from a reference

--- piggyback-learning-frontend/src/components/Video.js
+++ piggyback-learning-frontend/src/components/Video.js
@@ -1,61 +1,67 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+// previous code was bad (it would stop the video, not pause)
+// basically ripped https://codesandbox.io/p/sandbox/react-youtube-play-pause-video-using-an-external-button-c77o1v?file=%2Fsrc%2FApp.tsx%3A19%2C18
+// but removed the typescript annotations (I fought with chatgpt for like 30 min)
+// i'm familiar with Effect Hooks but this is more complicated than anything I'd used before
+
+
+// used npm install react-youtube
+
+import { useState, useEffect, useRef } from "react";
+import YouTube from "react-youtube";
 import '../styles/video.css';
-import { Container } from 'react-bootstrap';
-import {useRef} from "react";
 
 
-// cd piggyback-learning-frontend
-// npm install react-bootstrap bootstrap
+export default function App() {
+  const [isPaused, setIsPaused] = useState(false);
+  const videoRef = useRef(null);
 
-
-// used https://youtu.be/xNRJwmlRBNU as a tutorial for the embed
-
-
-const Video = () => {
-
-const videoRef = useRef(null);
-
-const stopVideos = () => {
-    if (videoRef.current) {
-      videoRef.current.src = ""; // Clear the src
-      setTimeout(() => {
-        videoRef.current.src = "https://www.youtube.com/embed/DR-cfDsHCGA";
-      }, 100); // last argument is a delay but don't remove it because it causes the embed to disapear 
-    }
-    
+  const togglePause = () => {
+    setIsPaused((prev) => !prev);
   };
 
+  useEffect(() => {
+    if (videoRef.current) {
+      const elapsed_seconds = videoRef.current.getCurrentTime();
+      console.log(`Current time: ${elapsed_seconds}s`);
+
+      if (isPaused) {
+        videoRef.current.pauseVideo();
+      } else {
+        videoRef.current.playVideo();
+      }
+    }
+  }, [isPaused]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (videoRef.current && videoRef.current.getCurrentTime() > 0) {
+        console.log(`Current time: ${videoRef.current.getCurrentTime()}s`);
+        const playerState = videoRef.current.getPlayerState();
+        console.log(playerState === 1 ? "Video is playing" : "Video is paused");
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const _onReady = (event) => {
+    videoRef.current = event.target;
+  };
 
   return (
     <div>
-      <header>
-        <h1>Piggyback Learning</h1>
-        <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/how-to-join">How to Join</Link></li>
-            <li><Link to="/store">Store</Link></li>
-          </ul>
-        </nav>
-      </header>
-
-      <main>
-        <Container>
-            <div class="ratio ratio-16x9">
-                <iframe  ref={videoRef} src="https://www.youtube.com/embed/DR-cfDsHCGA" title="YouTube video" allowFullScreen></iframe>
-            </div>
-            <button onClick={stopVideos}>Stop</button>;
-        </Container>
-       
-      </main>
-
-      <footer>
-        <p>&copy; 2025 Piggyback Learning. All Rights Reserved.</p>
-      </footer>
+      <div className="yvid">
+        <YouTube 
+          videoId={"DR-cfDsHCGA"} // we can add a variable here later when re-using this page
+          opts={{
+            height: "390",
+            width: "640",
+            playerVars: {autoplay: 1,},}} 
+          onReady={_onReady} 
+        />
+      </div>
+      <button onClick={togglePause}>{isPaused ? "Play" : "Pause"}</button>
     </div>
   );
-};
-
-export default Video;
+}
 
