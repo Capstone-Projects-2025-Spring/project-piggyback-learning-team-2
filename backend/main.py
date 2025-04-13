d05b565507966fb048924ffc5998f0a5adbb33a4
TonyGao <tuo60854@temple.edu>
2025-04-13 11:37:51 -0400

    Add middleware

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,28 @@
-from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi import FastAPI, HTTPException, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from . import db_models, schemas, tools
 from sqlalchemy.orm import Session
 from .database import engine, get_db
 from .youtube import retreiveYoutubeMetaData
 from .routers import crud_test, authentication, videos
 
-models.Base.metadata.create_all(bind=engine)
+db_models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["https://branma-front-latest.onrender.com/"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(crud_test.router)
 app.include_router(authentication.router)
 app.include_router(videos.router)
-
-
-# transition to use sqlalchemy
-@app.get("/sqlalchemy")
-def test_url(db: Session = Depends(get_db)):
-    engagement = db.query(models.User_engagment).all()
-    return {"data": engagement}
 
 
 @app.post("/youtube_metadata")
@@ -47,26 +51,3 @@ def get_youtube_metadata(video: schemas.YouTubeVideo):
         )
     metadata = retreiveYoutubeMetaData(video_id)
     return {"metadata": metadata}
-
-
-# testing out user registration
-@app.post("/register", status_code=status.HTTP_201_CREATED,
-          response_model=schemas.UserResponse)
-def register_user(user: schemas.UserCredentials,
-                  db: Session = Depends(get_db)):
-    existing_user = (db.query(models.User_Login)
-                     .filter(models.User_Login.email == user.email)
-                     .first())
-    if existing_user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail="Email already exists"
-        )
-    hashed_password = tools.hash(user.password)
-    user.password = hashed_password
-    new_user = models.User_Login(**user.model_dump())
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-
-    return new_user
--- backend/routers/authentication.py
+++ backend/routers/authentication.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import db_models, tools, oauth2
+from .. import db_models, tools, oauth2, schemas
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 
 router = APIRouter()
@@ -25,3 +25,26 @@ def login(userCredentials: OAuth2PasswordRequestForm = Depends(),
                             detail="Wrong login information")
     access_token = oauth2.create_access_token(data={"id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+# testing out user registration
+@router.post("/register", status_code=status.HTTP_201_CREATED,
+          response_model=schemas.UserResponse)
+def register_user(user: schemas.UserCredentials,
+                  db: Session = Depends(get_db)):
+    existing_user = (db.query(db_models.User_Login)
+                     .filter(db_models.User_Login.email == user.email)
+                     .first())
+    if existing_user:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already exists"
+        )
+    hashed_password = tools.hash(user.password)
+    user.password = hashed_password
+    new_user = db_models.User_Login(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
